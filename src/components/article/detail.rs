aef942a25898c83f810bf7126c30f961c6d26f5f
Lucas chen <lucas.chen@gbo-asia.com>
2024-06-18 17:32:42 +0800

    complete article detail

--- src/components/article/detail.rs
+++ src/components/article/detail.rs
@@ -4,22 +4,29 @@ use dioxus::prelude::*;
 pub fn Detail(article_id: u64) -> Element {
     tracing::info!("Article detail");
 
-    let markdown_body = include_str!("../../../README.md");
-    
-    rsx! {
-        section { class: "pb-5",
-            div { class: "flex justify-between",
-                h1 { class: "text-xl font-semibold",
-                    "Rust 中智能指针的学习记录（一）"
-                }
-                span { class: "text-gray-500 text-lg",
-                    "March 21, 2024"
-                }
-            }
+    let mut options = pulldown_cmark::Options::empty();
+    options.insert(pulldown_cmark::Options::ENABLE_TABLES);
+    let parser = pulldown_cmark::Parser::new_ext(include_str!("../../../README.md"), options);
+    let mut html_output = String::new();
+    pulldown_cmark::html::push_html(&mut html_output, parser);
 
-            // FIXME: Fix markdown html showing.
-            article { class: "py-3 markdown-body dioxus-blog-post",
-                dangerous_inner_html: format_args!("{}", markdown_body)
+    rsx! {
+        section { class: "max-w-screen-xl",
+            div {
+                div { class: "flex justify-between py-10",
+                    h1 { class: "text-xl font-semibold",
+                        "Rust 中智能指针的学习记录（一）"
+                    }
+                    span { class: "text-gray-500 text-lg",
+                        "March 21, 2024"
+                    }
+                }
+
+                div {
+                    article { class: "w-full py-3 prose",
+                        dangerous_inner_html: "{html_output}"
+                    }
+                }
             }
         }
     }
